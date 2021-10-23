86adfbc4aaf03978f093daf626543a7f9c900814
Zachary Meyers <zacharybmeyers@gmail.com>
2021-10-22 17:37:41 -0700

    notes can now be uploaded to the server as .ogg files, and downloaded again via GET request

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
-var handlebars = require('express-handlebars').create({defaultLayout:'main'});;
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 var multer = require('multer');
+var fs = require('fs');
+var path = require('path');
 
 var app = express();
 var upload = multer({ dest: "audio/" });
@@ -25,8 +27,22 @@ app.get('/', (req, res) => {
 app.post('/upload', type, (req, res) => {
     console.log(req.body);
     console.log(req.file);
-    // do stuff with file here
-    res.send('file received by server');
+    // do stuff with file here - store in DB?
+    // for now multer will process FormData and store in /audio
+    res.send('blob received by server');
+})
+
+app.get('/download/:fileName', (req, res) => {
+    res.sendFile(path.resolve(`./audio/${req.params.fileName}.ogg`));
+    // res.download(path.resolve(`./audio/${req.params.fileName}.ogg`));
+    // res.attachment(path.resolve(`./audio/${req.params.fileName}.ogg`));
+})
+
+app.post('/upload/base64', (req, res) => {
+    // strip metadata from audio string, write to .ogg file
+    var rawAudioString = req.body.audioString.replace('data:audio/webm;codecs=opus;base64,', '')
+    fs.writeFileSync(`./audio/${req.body.name}.ogg`, Buffer.from(rawAudioString, 'base64'));
+    res.send('base64 received by server');
 })
 
 app.listen(app.get('port'), function () {
--- public/index.js
+++ public/index.js
@@ -53,3 +53,44 @@ async function sendBlob(blob, note) {
 }
 
 document.getElementById("submit").addEventListener('click', () => createRecording("C4"));
+
+
+function createRecording2(note) {
+    const recorder = new Tone.Recorder();
+    const synth = new Tone.Synth().connect(recorder);
+    // start recording
+    recorder.start();
+    // generate a note
+    synth.triggerAttackRelease(note, "8n");
+    // wait for the notes to end and stop the recording
+    setTimeout(async () => {
+        // the recorded audio is returned as a blob
+        const recordingBlob = await recorder.stop();
+        var reader = new FileReader();
+        reader.readAsDataURL(recordingBlob);
+        reader.onloadend = () => {
+            var base64data = reader.result;
+            sendBase64(base64data, note).then(text => console.log(text)).catch(err => console.log(err));
+        }
+    }, 4000);
+}
+
+async function sendBase64(base64data, note) {
+    // myBlob = new Blob(["This is my blob content"], {type : "text/plain"});
+    // wrap recordingBlob in FormData
+    let options = {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            name: note,
+            audioString: base64data
+        })
+    }
+    let response = await fetch("/upload/base64", options);
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return await response.text();
+}
+
+document.getElementById("submit2").addEventListener('click', () => createRecording2("C4"));
