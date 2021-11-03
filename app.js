e3fc6c5081260b49b487ad577fc3dfc83ec298a9
Zachary Meyers <zacharybmeyers@gmail.com>
2021-11-03 00:25:54 -0700

    mongodb client wraps express endpoints

--- app.js
+++ app.js
@@ -4,17 +4,6 @@ var multer = require('multer');
 var fs = require('fs');
 var path = require('path');
 
-const sqlite = require('sqlite3');
-const db = new sqlite.Database('./user.sqlite', (err) => console.log(err));
-db.serialize(() => {
-    db.run(`CREATE TABLE IF NOT EXISTS user (
-        id INTEGER PRIMARY KEY AUTOINCREMENT, 
-        userID INT, 
-        patchName varchar, 
-        noteName varchar, 
-        rawAudioString varchar)`);
-})
-
 var app = express();
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -31,86 +20,74 @@ var upload = multer({ dest: "audio/" });
 var type = upload.single('upl');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
+// MongoDB setup
+var MongoClient = require('mongodb').MongoClient;
+const username = 'zsynth';
+const password = 'zsynth';
+const uri = `mongodb+srv://${username}:${password}@cluster0.bzqjc.mongodb.net/zsynth_users?retryWrites=true&w=majority`;
 
-app.get('/login', (req, res) => {
-    res.render('login');
-})
+MongoClient.connect(uri)
+.then(client => {
+    console.log('connected to mongodb');
+    const db = client.db("zsynth");
 
-// single file GET request
-app.get('/download/:patchName/:fileName', (req, res) => {
-    res.sendFile(path.resolve(`./audio/${req.params.patchName}/${req.params.fileName}.ogg`));
-    // res.download(path.resolve(`./audio/${req.params.fileName}.ogg`));
-    // res.attachment(path.resolve(`./audio/${req.params.fileName}.ogg`));
-})
-
-// // TODO: refactor for lighter DB storage -> store rawAudioString in DB, only convert to .ogg file when GET request comes in
-// app.post('/upload/base64', (req, res) => {
-//     // strip metadata from audio string, write to .ogg file
-//     var rawAudioString = req.body.audioString.replace('data:audio/webm;codecs=opus;base64,', '')
-//     fs.writeFileSync(`./audio/${req.body.parentDir}/${req.body.name}.ogg`, Buffer.from(rawAudioString, 'base64'));
-//     res.send('base64 received by server');
-// })
-
-app.post('/upload/base64', (req, res) => {
-    // strip metadata from audio string, write to .ogg file
-    var rawAudioString = req.body.audioString.replace('data:audio/webm;codecs=opus;base64,', '')
-    var stmt = db.prepare('INSERT INTO user (userID, patchName, noteName, rawAudioString) VALUES (?, ?, ?, ?)');
-    stmt.run(333, req.body.parentDir, req.body.name, rawAudioString);
-    stmt.finalize();
-    res.send('base64 added to db');
-})
-
-
-// TODO: keep trying to get download working with database
-app.get('/download/:userID/:patchName/:noteName', (req, res) => {
-    var row = getAudioFromDB(req.params.userID, req.params.patchName, req.params.noteName);
-    console.log(row);
-    // audio found, create temp file
-    fs.writeFileSync(`./audio/${row.noteName}.ogg`, Buffer.from(row.rawAudioString, 'base64'));
-    console.log(`successfully created ./audio/${row.noteName}.ogg`)
-    // send to client
-    res.sendFile(path.resolve(`./audio/${row.noteName}.ogg`), (err) => {
-        if (err) {
-            console.log(err);
-        }
-        // delete temp file
-        fs.unlink(`./audio/${row.noteName}.ogg`, (err) => {
-            if (err) throw err;
-            console.log(`successfully deleted ./audio/${row.noteName}.ogg`);
-        })
-    });
-})
-
-function getAudioFromDB(userID, patchName, noteName) {
-    var sql = `SELECT rawAudioString, noteName 
-    FROM user 
-    WHERE userID = ? 
-    AND patchName = ?
-    AND noteName = ?`
-    db.get(sql, [userID, patchName, noteName], (err, row) => {
-        return row;
+    // main server code here
+    app.get('/', (req, res) => {
+        res.render('home');
     })
-}
+    
+    app.get('/login', (req, res) => {
+        res.render('login');
+    })
 
-app.post('/patch', type, (req, res) => {
-    fs.mkdirSync(path.resolve(`./audio/${req.body.pname}`), { recursive: true })
-    res.end();
+    app.get('/createUser', (req, res) => {
+        db.collection("users").insertOne({user_id: 1});
+        res.send('user added to db!');
+    })
+
+    // mongodb method for note upload
+    app.post('/upload/base64', (req, res) => {
+        // mongo.createUser()?
+    
+        // strip metadata from audio string, write to .ogg file
+        var rawAudioString = req.body.audioString.replace('data:audio/webm;codecs=opus;base64,', '')
+    
+        res.send('base64 added to db');
+    })
+
+    // single file GET request
+    app.get('/download/:patchName/:fileName', (req, res) => {
+        res.sendFile(path.resolve(`./audio/${req.params.patchName}/${req.params.fileName}.ogg`));
+        // res.download(path.resolve(`./audio/${req.params.fileName}.ogg`));
+        // res.attachment(path.resolve(`./audio/${req.params.fileName}.ogg`));
+    })
+    
+    // // TODO: refactor for lighter DB storage -> store rawAudioString in DB, only convert to .ogg file when GET request comes in
+    // app.post('/upload/base64', (req, res) => {
+    //     // strip metadata from audio string, write to .ogg file
+    //     var rawAudioString = req.body.audioString.replace('data:audio/webm;codecs=opus;base64,', '')
+    //     fs.writeFileSync(`./audio/${req.body.parentDir}/${req.body.name}.ogg`, Buffer.from(rawAudioString, 'base64'));
+    //     res.send('base64 received by server');
+    // })
+    
+    app.post('/patch', type, (req, res) => {
+        fs.mkdirSync(path.resolve(`./audio/${req.body.pname}`), { recursive: true })
+        res.end();
+    })
+    
+    // error handling
+    app.use( (req, res) => {
+        res.status(404);
+        // res.render('404');
+        });
+        
+        app.use( (req, res) => {
+        res.status(500);
+        // res.render('500');
+        });
+    
+    app.listen(app.get('port'), function () {
+        console.log(`Express started on http://localhost:${app.get('port')}; press Ctrl-C to terminate.`)
+    })
 })
-
-// error handling
-app.use( (req, res) => {
-    res.status(404);
-    // res.render('404');
-  });
-  
-  app.use( (req, res) => {
-    res.status(500);
-    // res.render('500');
-  });
-
-app.listen(app.get('port'), function () {
-    console.log(`Express started on http://localhost:${app.get('port')}; press Ctrl-C to terminate.`)
-})
+.catch(e => console.error(e))
--- mongo.js
+++ mongo.js
@@ -1,13 +1,15 @@
 const { MongoClient } = require('mongodb');
 
 async function main() {
+    const username = 'zsynth';
     const password = 'zsynth';
-    const uri = `mongodb+srv://zsynth:${password}@cluster0.bzqjc.mongodb.net/zsynth_users?retryWrites=true&w=majority`;
+    const uri = `mongodb+srv://${username}:${password}@cluster0.bzqjc.mongodb.net/zsynth_users?retryWrites=true&w=majority`;
 
     const client = new MongoClient(uri);
 
     try {
         await client.connect();
+        const db = client.db("zsynth");
 
         const user = {
             user_id: 1
@@ -23,9 +25,9 @@ async function main() {
             audio_string: "alskjekwqjlrjqewr"
         }
 
-        await createUser(client, user);
-        await addPatchToUser(client, user.user_id, patch);
-        await addNoteToPatch(client, user.user_id, patch.patch_name, note);
+        await createUser(db, user);
+        await addPatchToUser(db, user.user_id, patch);
+        await addNoteToPatch(db, user.user_id, patch.patch_name, note);
 
     } catch(e) {
         console.error(e);
@@ -37,23 +39,23 @@ async function main() {
 
 main().catch(console.error);
 
-async function createUser(client, user) {
-    const result = await client.db("zsynth").collection("users").insertOne(user);
+async function createUser(db, user) {
+    const result = await db.collection("users").insertOne(user);
     console.log(`new user id: ${result.insertedId}`);
 }
 
 // mongosh command to delete all documents from "users" collection: db.users.deleteMany({})
 
-async function addPatchToUser(client, userID, patch) {
-    const result = await client.db("zsynth").collection("users").updateOne(
+async function addPatchToUser(db, userID, patch) {
+    const result = await db.collection("users").updateOne(
         { user_id: userID },
         { $push: { patches: patch } }
     );
     console.log(`patch: ${patch.patch_name} added`);
 }
 
-async function addNoteToPatch(client, userID, patchName, note) {
-    const result = await client.db("zsynth").collection("users").updateOne(
+async function addNoteToPatch(db, userID, patchName, note) {
+    const result = await db.collection("users").updateOne(
         { user_id: userID, 
         "patches.patch_name": patchName },
         { $push: { "patches.$.notes": note } }
